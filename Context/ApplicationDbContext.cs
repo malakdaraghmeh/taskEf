6bb53133dea021df836df55e24db75523de5d252
malakdaraghmeh <malakdaraghmeh23@gmail.com>
2024-08-05 01:18:53 +0300

    Add project files.

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using taskEf.Models;
+
+namespace taskEf.Context
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server = .; Database = TaskEF ; Trusted_Connection=True; TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
